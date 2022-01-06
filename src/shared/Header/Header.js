cb8a58f8437acf7d6452351bdfbf6d7a36239dee
unknown <sourabahmed546@gmail.com>
2022-01-07 02:08:14 +0600

    header-updated

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -12,11 +12,11 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg ">
             <div className="container-fluid logo">
                 <img className="img-fluid" src={Logo} alt="" />
-                <button className="navbar-toggler navbar-light " type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler navbar-dark " type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon "></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav ms-sm-0 ms-lg-5 me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link to="/home" className="nav-link active link-color" aria-current="page">Home</Link>
                         </li>
@@ -34,11 +34,11 @@ const Header = () => {
                     </ul>
                     <div className="navbar-text d-flex align-items-center">
                         <div className="dashboard">
-                            <h5>{user.email? user.displayName: ""}</h5>
+                            <h6><span>{user.email? "Hello, ":""}</span>{user.email? user.displayName: ""}</h6>
                         </div>
                         <div className="login">
                             {
-                                user.email? <button onClick={() => logOut()} className="logout-btn">Logout<i className="fas fa-sign-in-alt"></i></button>:
+                                user.email? <Link onClick={logOut} to="/login">Logout <i className="fas fa-sign-in-alt"></i></Link>:
                                 <Link to="/login">Login <i className="fas fa-sign-in-alt"></i></Link>
                             }
                         </div>
